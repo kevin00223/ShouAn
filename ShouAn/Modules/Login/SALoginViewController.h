99ffcd5cbbdbca424206bae1a7b910c0af633a6c
李凯 <likai.new@gmail.com>
2019-06-11 15:27:10 +0800

    添加登录控制器

--- ShouAn/Modules/Login/SALoginViewController.h
+++ ShouAn/Modules/Login/SALoginViewController.h
@@ -7,11 +7,10 @@
 //
 
 #import "SABaseViewController.h"
-#import "WMPageController.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SALoginViewController : WMPageController <WMPageControllerDataSource, WMPageControllerDelegate>
+@interface SALoginViewController : SABaseViewController
 
 @end
 
